2203e5cdc9f939fe5a3096048129f7f61044ab08
the-degenerate-otaku <136910781+the-degenerate-otaku@users.noreply.github.com>
2026-07-04 14:32:44 +0530

    Pong working and sound timer working

--- index.js
+++ index.js
@@ -4,9 +4,32 @@ const canvas = document.getElementById('chip8-canvas');
 const chip8 = new Chip8();
 const display = new Display(canvas, 10);
 const input = new Input(chip8.keys);
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let oscillator = null;
+
+function startBeep() {
+    if (oscillator) return;
+    oscillator = audioCtx.createOscillator()
+    oscillator.type = 'square'
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+    oscillator.connect(audioCtx.destination);
+    oscillator.start();
+}
+
+function stopBeep() {
+    if (!oscillator) return;
+    oscillator.stop();
+    oscillator.disconnect();
+    oscillator = null;
+}
 
 setInterval(() => {
     chip8.updateTimer();
+    if (chip8.soundtimer > 0) {
+        startBeep();
+    } else {
+        stopBeep();
+    }
 }, 1000 / 60);
 
 function loop() {
@@ -18,7 +41,7 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
-fetch('roms/4-flags.ch8')
+fetch('roms/Pong.ch8') // games actually work tf T-T
     .then(res => res.arrayBuffer())
     .then(Buffer => {
         chip8.loadRom(Buffer);
